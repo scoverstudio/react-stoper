ca8c9aebcedbd3e136944d037370835b5e3e56be
Krystian Matkowski <scoverstudio@gmail.com>
2021-12-30 15:28:57 +0100

    full working timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Timer from "./components/Timer/Timer";
+import Button from "./components/Button/Button";
+import { useState, useEffect } from "react";
+import styles from "../src/App.module.scss";
+
+const TimerContainer = () => {
+  const [time, setTime] = useState(0)
+  const [timer, setTimer] = useState(null)
+
+  const stopTimer = () => {
+    if(timer) clearInterval(timer);
+  };
+
+  const start = () => {
+    stopTimer();
+    setTimer(
+      setInterval(() => {
+        setTime(time => time + 1);
+      }, 1)
+    );
+  };
+
+  const resetTimer = () => {
+    setTime(time => 0)
+    stopTimer();
+  };
+
+  useEffect(() => {
+    return () => {
+      stopTimer();
+    };
+  }, []);
+
+
+  return (
+    <div className={styles.timer}>
+      <Timer time={time}/>
+      <Button onClick={start}>START</Button>
+      <Button onClick={stopTimer}>STOP</Button>
+      <Button onClick={resetTimer}>RESET</Button>
+    </div>
+  );
+}
 
 function App() {
+  const [showTimer, setShowTimer] = useState(true);
+  const toggleTimer = () => setShowTimer(shouldShow => !shouldShow);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      {showTimer && <TimerContainer />}
+      <Button onClick={toggleTimer}>{showTimer ? "hide" : "show"}</Button>
     </div>
   );
 }
